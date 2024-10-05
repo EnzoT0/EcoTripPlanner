a7dfbf23afc4a68209c48ca429af55c4aed33819
Jacky Feng <jfeng14@students.cs.ubc.ca>
2024-10-05 14:57:05 -0700

    form_redesign2

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Map from './Map.tsx';
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
